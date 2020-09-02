0ff53d5a3a64b350e39ba4e41765dd5e44d2b4a9
Filip Oščádal <f@mxd.cz>
2020-09-02 09:50:24 +0200

    + branch, minor changes

--- build.sh
+++ build.sh
@@ -11,10 +11,8 @@ command -v docker >/dev/null 2>&1 || fail "Docker is NOT installed!"
 if [ ! -n $(id -Gn "$(whoami)" | grep -c "docker") ]
     then if [ "$(id -u)" != "0" ]; then fail "Add yourself to the 'docker' group or run this script as root!"; fi
 fi
-
 if [ ! -r ".env" ]; then fail "Missing .env file!"; fi
 export $(grep -v '^#' .env | xargs -d '\n')
-
 if [ -z "$TAG" ]; then fail "Missing TAG definition!"; fi
 
 docker build --pull -t $TAG .
--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,10 @@
 cd "$(dirname "$0")"
 export PATH=$PATH:/snap/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+command -v docker >/dev/null 2>&1 || {
+    echo -en "\nDocker is NOT installed!\n\n"
+    exit 1
+}
+
 docker rm searx --force
 docker run -d -p 8888:8888 --restart unless-stopped --name searx gscloudcz/searx
